ce3cc9c0f7fb93276548ffa4b89515326507dd23
gavinraney <85324062+gavinraney@users.noreply.github.com>
2022-05-03 16:42:17 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
-
+function myFunction() {
+  var x = document.getElementById("suggestions");
+  if (x.className === "suggestions") {
+    x.className += " responsive";
+  } else {
+    x.className = "suggestions";
+  }
+}
